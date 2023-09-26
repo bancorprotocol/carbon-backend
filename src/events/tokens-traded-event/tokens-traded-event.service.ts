98e55bcd4a3a9e752b868113bdad0b7902d43eb2
John Doe <yariv@bancor.network>
2023-09-26 17:41:33 +0200

    generelized get method

--- src/events/tokens-traded-event/tokens-traded-event.service.ts
+++ src/events/tokens-traded-event/tokens-traded-event.service.ts
@@ -16,7 +16,11 @@ type TokensTradedEventQueryParams = {
   limit?: number;
   type?: string;
   pairId?: number;
+  last24h?: boolean;
+  order?: QueryOrder; // ASC is default
 };
+
+type QueryOrder = 'ASC' | 'DESC';
 
 @Injectable()
 export class TokensTradedEventService {
@@ -61,7 +65,8 @@ export class TokensTradedEventService {
   }
 
   async get(params: TokensTradedEventQueryParams = {}): Promise<TokensTradedEvent[]> {
-    const { startBlock, endBlock, startTime, endTime, limit, type, pairId } = params;
+    const { startBlock, endBlock, startTime, endTime, limit, type, pairId, last24h, order } = params;
+    const queryOrder = order === 'DESC' ? 'DESC' : 'ASC';
 
     const queryBuilder = this.repository
       .createQueryBuilder('tokensTradedEvents')
@@ -71,7 +76,7 @@ export class TokensTradedEventService {
       .leftJoinAndSelect('tokensTradedEvents.block', 'block')
       .leftJoinAndSelect('tokensTradedEvents.sourceToken', 'sourceToken')
       .leftJoinAndSelect('tokensTradedEvents.targetToken', 'targetToken')
-      .orderBy('block.id', 'ASC');
+      .orderBy('block.id', queryOrder);
 
     if (startBlock !== undefined) {
       queryBuilder.andWhere('block.id > :startBlock', { startBlock });
@@ -97,41 +102,32 @@ export class TokensTradedEventService {
       queryBuilder.andWhere('pair.id = :pairId', { pairId });
     }
 
+    if (last24h === true) {
+      queryBuilder.where(`tokensTradedEvents.timestamp >= NOW() - INTERVAL '24 hours'`);
+    }
+
     if (limit !== undefined) {
       queryBuilder.take(limit);
     }
 
     const trades = await queryBuilder.getMany();
 
+    // normalize amounts
+    trades.forEach((t) => {
+      t.sourceAmount = new Decimal(t.sourceAmount).div(`1e${t.sourceToken.decimals}`).toString();
+      t.targetAmount = new Decimal(t.targetAmount).div(`1e${t.targetToken.decimals}`).toString();
+      if (t.byTargetAmount) {
+        t.tradingFeeAmount = new Decimal(t.tradingFeeAmount).div(`1e${t.sourceToken.decimals}`).toString();
+      } else {
+        t.tradingFeeAmount = new Decimal(t.tradingFeeAmount).div(`1e${t.targetToken.decimals}`).toString();
+      }
+    });
+
     return trades;
   }
 
-  async all(limit = 0, ascending = true): Promise<TokensTradedEvent[]> {
-    const order = ascending ? 'ASC' : 'DESC';
-    let trades = this.repository
-      .createQueryBuilder('tokensTradedEvents')
-      .leftJoinAndSelect('tokensTradedEvents.block', 'block')
-      .leftJoinAndSelect('tokensTradedEvents.pair', 'pair')
-      .leftJoinAndSelect('tokensTradedEvents.sourceToken', 'sourceToken')
-      .leftJoinAndSelect('tokensTradedEvents.targetToken', 'targetToken')
-      .orderBy('block.id', order);
-
-    if (limit > 0) {
-      trades = trades.take(limit);
-    }
-
-    return trades.getMany();
-  }
-
   async volume24hByToken(): Promise<any> {
-    const trades = await this.repository
-      .createQueryBuilder('tokensTradedEvents')
-      .leftJoinAndSelect('tokensTradedEvents.block', 'block')
-      .leftJoinAndSelect('tokensTradedEvents.pair', 'pair')
-      .leftJoinAndSelect('tokensTradedEvents.sourceToken', 'sourceToken')
-      .leftJoinAndSelect('tokensTradedEvents.targetToken', 'targetToken')
-      .where(`tokensTradedEvents.timestamp >= NOW() - INTERVAL '24 hours'`)
-      .getMany();
+    const trades = await this.get({ last24h: true });
 
     const result = {};
     trades.forEach((t) => {
@@ -143,29 +139,16 @@ export class TokensTradedEventService {
         result[t.targetToken.id] = new Decimal(0);
       }
 
-      result[t.sourceToken.id] = new Decimal(result[t.sourceToken.id]).add(
-        new Decimal(t.sourceAmount).div(`1e${t.sourceToken.decimals}`),
-      );
+      result[t.sourceToken.id] = new Decimal(result[t.sourceToken.id]).add(new Decimal(t.sourceAmount));
 
-      result[t.targetToken.id] = new Decimal(result[t.targetToken.id]).add(
-        new Decimal(t.targetAmount).div(`1e${t.targetToken.decimals}`),
-      );
+      result[t.targetToken.id] = new Decimal(result[t.targetToken.id]).add(new Decimal(t.targetAmount));
     });
 
     return result;
   }
 
   async volume24hByPair(): Promise<any> {
-    const trades = await this.repository
-      .createQueryBuilder('tokensTradedEvents')
-      .leftJoinAndSelect('tokensTradedEvents.block', 'block')
-      .leftJoinAndSelect('tokensTradedEvents.pair', 'pair')
-      .leftJoinAndSelect('pair.token0', 'token0')
-      .leftJoinAndSelect('pair.token1', 'token1')
-      .leftJoinAndSelect('tokensTradedEvents.sourceToken', 'sourceToken')
-      .leftJoinAndSelect('tokensTradedEvents.targetToken', 'targetToken')
-      .where(`tokensTradedEvents.timestamp >= NOW() - INTERVAL '24 hours'`)
-      .getMany();
+    const trades = await this.get({ last24h: true });
 
     const result = {};
     trades.forEach((t) => {
@@ -177,21 +160,13 @@ export class TokensTradedEventService {
       }
 
       if (t.pair.token0.id === t.sourceToken.id) {
-        result[t.pair.id].token0Volume = result[t.pair.id].token0Volume.add(
-          new Decimal(t.sourceAmount).div(`1e${t.sourceToken.decimals}`),
-        );
+        result[t.pair.id].token0Volume = result[t.pair.id].token0Volume.add(new Decimal(t.sourceAmount));
 
-        result[t.pair.id].token1Volume = result[t.pair.id].token1Volume.add(
-          new Decimal(t.targetAmount).div(`1e${t.targetToken.decimals}`),
-        );
+        result[t.pair.id].token1Volume = result[t.pair.id].token1Volume.add(new Decimal(t.targetAmount));
       } else {
-        result[t.pair.id].token0Volume = result[t.pair.id].token0Volume.add(
-          new Decimal(t.targetAmount).div(`1e${t.targetToken.decimals}`),
-        );
+        result[t.pair.id].token0Volume = result[t.pair.id].token0Volume.add(new Decimal(t.targetAmount));
 
-        result[t.pair.id].token1Volume = result[t.pair.id].token1Volume.add(
-          new Decimal(t.sourceAmount).div(`1e${t.sourceToken.decimals}`),
-        );
+        result[t.pair.id].token1Volume = result[t.pair.id].token1Volume.add(new Decimal(t.sourceAmount));
       }
     });
 
