abe0b76720965774a0360751c937849ddcb36312
John Doe <yariv@bancor.network>
2024-03-11 17:59:45 +0100

    removed old simulator

--- src/v1/v1.module.ts
+++ src/v1/v1.module.ts
@@ -6,10 +6,9 @@ import { MarketRateModule } from './market-rate/market-rate.module';
 import { V1Controller } from './v1.controller';
 import { DuneModule } from '../dune/dune.module';
 import { SimulatorModule } from './simulator/simulator.module';
-import { Simulator2Module } from './simulator2/simulator2.module';
 
 @Module({
-  imports: [CmcModule, RoiModule, CoingeckoModule, MarketRateModule, DuneModule, SimulatorModule, Simulator2Module],
+  imports: [CmcModule, RoiModule, CoingeckoModule, MarketRateModule, DuneModule, SimulatorModule],
   controllers: [V1Controller],
 })
 export class V1Module {}
