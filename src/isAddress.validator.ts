c740582131a29e5b4b102df0b3fb7756e45cda67
John Doe <yariv@bancor.network>
2024-03-26 14:16:40 +0100

    address formatting helper

--- src/isAddress.validator.ts
+++ src/isAddress.validator.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { registerDecorator, ValidationOptions } from 'class-validator';
 import { isAddress, toChecksumAddress } from 'web3-utils';
 
@@ -24,4 +25,16 @@ export function IsAddress(validationOptions?: ValidationOptions) {
       },
     });
   };
+}
+
+export function formatEthereumAddress(value): string {
+  try {
+    return toChecksumAddress(value.value);
+  } catch (error) {
+    throw new BadRequestException({
+      message: [`${value.key} must be a valid address, the value ${value.value} is invalid`],
+      error: 'Bad Request',
+      statusCode: 400,
+    });
+  }
 }
