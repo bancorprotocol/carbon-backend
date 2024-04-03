488b700ff5670a9e1ba755625f51a7aaf31e500c
John Doe <yariv@bancor.network>
2024-04-03 23:18:49 +0200

    added and improved analytics

--- src/v1/analytics/analytics.controller.ts
+++ src/v1/analytics/analytics.controller.ts
@@ -7,16 +7,23 @@ export class AnalyticsController {
   constructor(private analyticsService: AnalyticsService) {}
 
   @Get('tvl')
-  @CacheTTL(1 * 1000)
+  @CacheTTL(1 * 60 * 1000)
   @Header('Cache-Control', 'public, max-age=60')
   async tvl(): Promise<any> {
     return this.analyticsService.getCachedTVL();
   }
 
   @Get('volume')
-  @CacheTTL(1 * 1000)
+  @CacheTTL(1 * 60 * 1000)
   @Header('Cache-Control', 'public, max-age=60')
   async volume(): Promise<any> {
     return this.analyticsService.getCachedVolume();
+  }
+
+  @Get('generic')
+  @CacheTTL(1 * 60 * 1000)
+  @Header('Cache-Control', 'public, max-age=60')
+  async generic(): Promise<any> {
+    return this.analyticsService.getCachedGenericMetrics();
   }
 }
