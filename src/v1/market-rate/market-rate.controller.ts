459b837122361949b134a7b36c8d547de98bb0aa
John Doe <yariv@bancor.network>
2024-06-04 13:37:01 +0200

    updates sei token map

--- src/v1/market-rate/market-rate.controller.ts
+++ src/v1/market-rate/market-rate.controller.ts
@@ -28,9 +28,9 @@ export class MarketRateController {
     if (this.blockchainType === BlockchainType.Sei) {
       const seiMap = {
         '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee': 'sei-network',
-        '0xace5f7ea93439af39b46d2748fa1ac19951c8d7c': 'usd-coin',
-        '0xf983afa393199d6902a1dd04f8e93465915ffd8b': 'tether',
-        '0x027d2e627209f1ceba52adc8a5afe9318459b44b': 'sei-network',
+        '0x3894085ef7ff0f0aedf52e2a2704928d1ec074f1': 'usd-coin',
+        '0xb75d0b03c06a926e488e2659df1a861f860bd3d1': 'tether',
+        '0xe30fedd158a2e3b13e9badaeabafc5516e95e8c7': 'sei-network',
       };
       _address = seiMap[address.toLowerCase()];
       data = await this.coingeckoService.getCoinPrices([_address], currencies);
