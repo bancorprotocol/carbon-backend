31eca19ed280f3706398d3851c51c5d3f0ad405f
John Doe <yariv@bancor.network>
2024-06-04 14:29:28 +0200

    added url logging

---
+++ src/logging.middleware.ts
@@ -0,0 +1,13 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggingMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const controllerPath = req.path.split('/')[2];
+    const controllerName = controllerPath.charAt(0).toUpperCase() + controllerPath.slice(1) + 'Controller';
+    const logger = new Logger(controllerName);
+    logger.log(`URL: ${req.url}, Query Parameters: ${JSON.stringify(req.query)}`);
+    next();
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { LoggingMiddleware } from './logging.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableVersioning();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(new LoggingMiddleware().use);
 
   const config = new DocumentBuilder()
     .setTitle('Carbon API')
