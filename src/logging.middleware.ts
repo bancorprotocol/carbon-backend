7972f5f3d962dcc8e43e5e7f35ab9903c2353779
John Doe <yariv@bancor.network>
2024-06-06 13:15:12 +0200

    log cleanup

--- src/logging.middleware.ts
+++ src/logging.middleware.ts
@@ -5,6 +5,9 @@ import { Request, Response, NextFunction } from 'express';
 export class LoggingMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const controllerPath = req.path.split('/')[2];
+    if (!controllerPath) {
+      return next();
+    }
     const controllerName = controllerPath.charAt(0).toUpperCase() + controllerPath.slice(1) + 'Controller';
     const logger = new Logger(controllerName);
     logger.log(`URL: ${req.url}, Query Parameters: ${JSON.stringify(req.query)}`);
