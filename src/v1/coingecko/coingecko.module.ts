734aa008a6a1e338f4a46471ff6840793397157c
John Doe <yariv@bancor.network>
2024-08-28 17:26:32 +0200

    dune cleanup

--- src/v1/coingecko/coingecko.module.ts
+++ src/v1/coingecko/coingecko.module.ts
@@ -3,12 +3,11 @@ import { CoinGeckoController } from './coingecko.controller';
 import { CoingeckoService } from './coingecko.service';
 import { TokensTradedEventModule } from 'src/events/tokens-traded-event/tokens-traded-event.module';
 import { PairModule } from '../../pair/pair.module';
-import { DuneModule } from '../../dune/dune.module';
 import { StrategyModule } from '../../strategy/strategy.module';
 import { DeploymentModule } from '../../deployment/deployment.module';
 
 @Module({
-  imports: [TokensTradedEventModule, PairModule, DuneModule, StrategyModule, DeploymentModule],
+  imports: [TokensTradedEventModule, PairModule, StrategyModule, DeploymentModule],
   controllers: [CoinGeckoController],
   providers: [CoingeckoService],
   exports: [CoingeckoService],
