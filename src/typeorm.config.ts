46b8e893ea6c8757b860385db1770210c4d5d317
John Doe <yariv@bancor.network>
2024-09-04 15:48:57 +0200

    deployment setup

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -3,9 +3,27 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+const ssl =
+  process.env.NODE_ENV === 'production'
+    ? {
+        ca: process.env.CARBON_BACKEND_SQL_CERTIFICATION,
+        ciphers: [
+          'ECDHE-RSA-AES128-SHA256',
+          'DHE-RSA-AES128-SHA256',
+          'AES128-GCM-SHA256',
+          '!RC4', // RC4 be gone
+          'HIGH',
+          '!MD5',
+          '!aNULL',
+        ].join(':'),
+        honorCipherOrder: true,
+      }
+    : {};
+
 export default new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: ['**/*.entity.ts'], // Adjust the path to your entities
   migrations: ['dist/migrations/*.js'], // Path to compiled migration files
+  ssl,
 });
