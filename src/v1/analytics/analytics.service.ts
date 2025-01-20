02fb4387ae7200eda3da3611e6ecaefb9773483d
John Doe <yariv@bancor.network>
2025-01-20 15:01:51 +0100

    fixed incorrect pair assignment and join filters

--- src/v1/analytics/analytics.service.ts
+++ src/v1/analytics/analytics.service.ts
@@ -76,13 +76,13 @@ WITH filtered_strategies AS (
 ), strategies_with_decimals AS (
     SELECT fs.liquidity0, ts0.decimals AS decimals0, fs."token0Id", fs.liquidity1, ts1.decimals AS decimals1, fs."token1Id" 
     FROM filtered_strategies fs 
-    LEFT JOIN tokens ts0 ON fs."token0Id" = ts0.id 
-    LEFT JOIN tokens ts1 ON fs."token1Id" = ts1.id
+    LEFT JOIN tokens ts0 ON fs."token0Id" = ts0.id AND ts0."blockchainType" = '${deployment.blockchainType}' AND ts0."exchangeId" = '${deployment.exchangeId}'
+    LEFT JOIN tokens ts1 ON fs."token1Id" = ts1.id AND ts1."blockchainType" = '${deployment.blockchainType}' AND ts1."exchangeId" = '${deployment.exchangeId}'
 ), strategies_with_prices AS (
     SELECT swd.liquidity0::NUMERIC, decimals0::NUMERIC, swd."token0Id", q1.usd::NUMERIC AS price0, swd.liquidity1::NUMERIC, decimals1::NUMERIC, swd."token1Id", q2.usd::NUMERIC AS price1 
     FROM strategies_with_decimals swd 
-    LEFT JOIN quotes q1 ON swd."token0Id" = q1."tokenId" 
-    LEFT JOIN quotes q2 ON swd."token1Id" = q2."tokenId"
+    LEFT JOIN quotes q1 ON swd."token0Id" = q1."tokenId" AND q1."blockchainType" = '${deployment.blockchainType}'
+    LEFT JOIN quotes q2 ON swd."token1Id" = q2."tokenId" AND q2."blockchainType" = '${deployment.blockchainType}'
 ), strategies_with_liquidity AS (
     SELECT (liquidity0 / POW(10, decimals0) * price0) AS liquidity 
     FROM strategies_with_prices 
@@ -118,8 +118,8 @@ WITH filtered_strategies AS (
 ), tokens_traded_with_token_info AS (
     SELECT tte."timestamp" AS timestamp, tte."transactionHash" AS transactionHash, tte."blockId" AS blockId, tte."trader" AS trader, tte."byTargetAmount" AS byTargetAmount, tte."sourceTokenId" AS sourceTokenId, tte."targetTokenId" AS targetTokenId, tte."sourceAmount" AS sourceAmount, tte."targetAmount" AS targetAmount, tte."tradingFeeAmount" AS tradingFeeAmount, ts."address" AS sourceAddress, ts."symbol" AS sourceSymbol, ts."decimals" AS sourceDecimals, tt."address" AS targetAddress, tt."symbol" AS targetSymbol, tt."decimals" AS targetDecimals 
     FROM "tokens-traded-events" tte 
-    JOIN tokens ts ON tte."sourceTokenId" = ts."id" 
-    JOIN tokens tt ON tte."targetTokenId" = tt."id"
+    JOIN tokens ts ON tte."sourceTokenId" = ts."id" AND ts."blockchainType" = '${deployment.blockchainType}' AND ts."exchangeId" = '${deployment.exchangeId}'
+    JOIN tokens tt ON tte."targetTokenId" = tt."id" AND tt."blockchainType" = '${deployment.blockchainType}' AND tt."exchangeId" = '${deployment.exchangeId}'
     WHERE tte."blockchainType" = '${deployment.blockchainType}' AND tte."exchangeId" = '${deployment.exchangeId}'
 
 ), correct_fee_units AS (
@@ -159,11 +159,14 @@ FROM sum_liquidity sl, strategies_created sc, pairs_created pc, unique_traders u
 
   private async getTradesCount(deployment: Deployment): Promise<any> {
     const query = `
-      SELECT "strategyId" as id, count(*) as trade_count
+      SELECT 
+        "strategyId" AS id, 
+        COUNT(*) AS trade_count
       FROM "strategy-updated-events" s
-      where reason = 1
-      AND "blockchainType" = '${deployment.blockchainType}' AND "exchangeId" = '${deployment.exchangeId}'
-      group by "strategyId"
+      WHERE "blockchainType" = '${deployment.blockchainType}'
+      AND "exchangeId" = '${deployment.exchangeId}'
+      AND reason = 1
+      GROUP BY "strategyId"
     `;
 
     const result = await this.strategy.query(query);
@@ -172,61 +175,124 @@ FROM sum_liquidity sl, strategies_created sc, pairs_created pc, unique_traders u
 
   private async getTrending(deployment: Deployment): Promise<any> {
     const totalTradeCountQuery = this.strategy.query(`
-      SELECT count(*)::INT as trade_count
+      SELECT 
+          COUNT(*)::INT AS trade_count
       FROM "strategy-updated-events"
-      WHERE "blockchainType" = '${deployment.blockchainType}' AND "exchangeId" = '${deployment.exchangeId}' AND "reason" = 1
+      WHERE "blockchainType" = '${deployment.blockchainType}'
+      AND "exchangeId" = '${deployment.exchangeId}'
+      AND "reason" = 1
     `);
 
     const tradeCountQuery = this.strategy.query(`
-      WITH strategy_trade_24hcounts as (
-          SELECT s."strategyId" as id, COUNT(s.*)::INT as strategy_trades_24h
-          FROM  "strategy-updated-events" s
-          WHERE s."timestamp" >= NOW() - INTERVAL '24 hours'
-          AND "blockchainType" = '${deployment.blockchainType}' AND "exchangeId" = '${deployment.exchangeId}' AND "reason" = 1
-          GROUP BY 1   
+      WITH strategy_trade_24hcounts AS (
+          SELECT 
+              s."blockchainType" AS "blockchainType", 
+              s."exchangeId" AS "exchangeId", 
+              s."strategyId" AS id, 
+              COUNT(s.*)::INT AS strategy_trades_24h
+          FROM "strategy-updated-events" s
+          WHERE s."timestamp" >= NOW() - INTERVAL '24 HOURS'
+          AND s."blockchainType" = '${deployment.blockchainType}'
+          AND s."exchangeId" = '${deployment.exchangeId}'
+          AND s."reason" = 1
+          GROUP BY 1, 2, 3   
       ),
-      strategy_trade_counts as (
-          SELECT s."strategyId" as id, COUNT(s.*)::INT as strategy_trades
-          FROM  "strategy-updated-events" s
-          WHERE "blockchainType" = '${deployment.blockchainType}' AND "exchangeId" = '${deployment.exchangeId}' AND "reason" = 1
-          GROUP BY 1   
+      strategy_trade_counts AS (
+          SELECT 
+              s."blockchainType"::TEXT AS "blockchainType", 
+              s."exchangeId"::TEXT AS "exchangeId", 
+              s."strategyId" AS id, 
+              COUNT(s.*)::INT AS strategy_trades
+          FROM "strategy-updated-events" s
+          WHERE s."blockchainType" = '${deployment.blockchainType}'
+          AND s."exchangeId" = '${deployment.exchangeId}'
+          AND s."reason" = 1
+          GROUP BY 1, 2, 3   
       )
-      SELECT stc.id, stc.strategy_trades, COALESCE(sc24.strategy_trades_24h,0) as strategy_trades_24h, t0.address as token0, t1.address as token1, 
-      t0.symbol as symbol0, t1.symbol as symbol1, 
-      t0.symbol || '/' || t1.symbol as pair_symbol,
-      t0.address || '/' || t1.address as pair_addresses
+      SELECT 
+          stc.id, 
+          stc.strategy_trades, 
+          COALESCE(sc24.strategy_trades_24h, 0) AS strategy_trades_24h, 
+          t0.address AS token0, 
+          t1.address AS token1, 
+          t0.symbol AS symbol0, 
+          t1.symbol AS symbol1, 
+          t0.symbol || '/' || t1.symbol AS pair_symbol,
+          t0.address || '/' || t1.address AS pair_addresses
       FROM strategy_trade_counts stc
-      LEFT JOIN "strategy-created-events" s ON s."strategyId" = stc.id
-      LEFT JOIN tokens t0 on t0.id = s."token0Id"
-      LEFT JOIN tokens t1 on t1.id = s."token1Id"
-      LEFT JOIN strategy_trade_24hcounts sc24 on sc24.id = stc.id
-      ORDER BY 2 DESC      
+      LEFT JOIN "strategy-created-events" s 
+          ON s."strategyId" = stc.id 
+      AND s."blockchainType"::TEXT = stc."blockchainType"::TEXT 
+      AND s."exchangeId"::TEXT = stc."exchangeId"::TEXT
+      LEFT JOIN tokens t0 
+          ON t0.id = s."token0Id" 
+      AND t0."blockchainType"::TEXT = stc."blockchainType"::TEXT 
+      AND t0."exchangeId"::TEXT = stc."exchangeId"::TEXT
+      LEFT JOIN tokens t1 
+          ON t1.id = s."token1Id" 
+      AND t1."blockchainType"::TEXT = stc."blockchainType"::TEXT 
+      AND t1."exchangeId"::TEXT = stc."exchangeId"::TEXT
+      LEFT JOIN strategy_trade_24hcounts sc24 
+          ON sc24.id = stc.id 
+      AND sc24."blockchainType" = stc."blockchainType"
+      AND sc24."exchangeId" = stc."exchangeId"
+      ORDER BY 2 DESC; 
     `);
 
     const pairCountQuery = this.strategy.query(`
-      WITH pair_trade_24hcounts as (
-          SELECT s."pairId" as pair_id, COUNT(s.*)::INT as pair_trades_24h
-          FROM  "strategy-updated-events" s
-          WHERE s."timestamp" >= NOW() - INTERVAL '24 hours'
-          AND "blockchainType" = '${deployment.blockchainType}' AND "exchangeId" = '${deployment.exchangeId}' AND "reason" = 1
-          GROUP BY 1   
+      WITH pair_trade_24hcounts AS (
+          SELECT 
+              s."blockchainType" AS "blockchainType", 
+              s."exchangeId" AS "exchangeId", 
+              s."pairId" AS pair_id, 
+              COUNT(s.*)::INT AS pair_trades_24h
+          FROM "strategy-updated-events" s
+          WHERE s."timestamp" >= NOW() - INTERVAL '24 HOURS'
+          AND s."blockchainType" = '${deployment.blockchainType}'
+          AND s."exchangeId" = '${deployment.exchangeId}'
+          AND s."reason" = 1
+          GROUP BY 1, 2, 3
       ),
-      pair_counts as (
-          SELECT s."pairId" as pair_id, COUNT(s.*)::INT as pair_trades
-          FROM  "strategy-updated-events" s
-          WHERE "blockchainType" = '${deployment.blockchainType}' AND "exchangeId" = '${deployment.exchangeId}' AND "reason" = 1
-          GROUP BY 1
+      pair_counts AS (
+          SELECT 
+              s."blockchainType" AS "blockchainType", 
+              s."exchangeId" AS "exchangeId", 
+              s."pairId" AS pair_id, 
+              COUNT(s.*)::INT AS pair_trades
+          FROM "strategy-updated-events" s
+          WHERE s."blockchainType" = '${deployment.blockchainType}'
+          AND s."exchangeId" = '${deployment.exchangeId}'
+          AND s."reason" = 1
+          GROUP BY 1, 2, 3
       )
-      SELECT p.pair_id, p.pair_trades, COALESCE(pc24.pair_trades_24h,0) as pair_trades_24h, 
-      pc.token0, pc.token1, t0.symbol as symbol0, t1.symbol as symbol1, 
-      t0.symbol || '/' || t1.symbol as pair_symbol,
-      pc.token0 || '/' || pc.token1 as pair_addresses
+      SELECT 
+          p.pair_id, 
+          p.pair_trades, 
+          COALESCE(pc24.pair_trades_24h, 0) AS pair_trades_24h, 
+          t0."address" as token0, 
+          t1."address" as token1, 
+          t0.symbol AS symbol0, 
+          t1.symbol AS symbol1, 
+          t0.symbol || '/' || t1.symbol AS pair_symbol,
+          t0."address" || '/' || t1."address" AS pair_addresses
       FROM pair_counts p
-      LEFT JOIN "pair-created-events" pc ON pc."id" = p.pair_id  
-      LEFT JOIN tokens t0 on t0.address = pc."token0"
-      LEFT JOIN tokens t1 on t1.address = pc."token1"
-      LEFT JOIN pair_trade_24hcounts pc24 on pc24.pair_id = p.pair_id
-      ORDER BY 2 DESC
+      LEFT JOIN "pairs" pc 
+          ON pc."id" = p.pair_id  
+      AND pc."blockchainType"::TEXT = p."blockchainType"::TEXT
+      AND pc."exchangeId"::TEXT = p."exchangeId"::TEXT
+      LEFT JOIN tokens t0 
+          ON t0.id = pc."token0Id" 
+      AND t0."blockchainType"::TEXT = p."blockchainType"::TEXT
+      AND t0."exchangeId"::TEXT = p."exchangeId"::TEXT
+      LEFT JOIN tokens t1 
+          ON t1.id = pc."token1Id" 
+      AND t1."blockchainType"::TEXT = p."blockchainType"::TEXT
+      AND t1."exchangeId"::TEXT = p."exchangeId"::TEXT
+      LEFT JOIN pair_trade_24hcounts pc24 
+          ON pc24.pair_id = p.pair_id
+      AND pc24."blockchainType" = p."blockchainType"
+      AND pc24."exchangeId" = p."exchangeId"
+      ORDER BY p.pair_trades DESC;
     `);
 
     const [totalTradeCount, tradeCount, pairCount] = await Promise.all([
