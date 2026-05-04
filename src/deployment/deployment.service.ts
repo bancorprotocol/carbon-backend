0c84b39e9061d0afcd4de1c6acbfba8053f3edfb
Liam Patterson <liam.patterson94@gmail.com>
2026-05-04 12:46:07 +0200

    reduced sei's block range

--- src/deployment/deployment.service.ts
+++ src/deployment/deployment.service.ts
@@ -163,7 +163,7 @@ export class DeploymentService {
         exchangeId: ExchangeId.OGSei,
         blockchainType: BlockchainType.Sei,
         rpcEndpoint: this.configService.get('SEI_RPC_ENDPOINT'),
-        harvestEventsBatchSize: 200,
+        harvestEventsBatchSize: 50,
         harvestConcurrency: 1,
         multicallAddress: '0x51aA24A9230e62CfaF259c47DE3133578cE36317',
         startBlock: 79146720,
