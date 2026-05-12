6768e52a566e9199c91c5d27a6ec1a418d5fa967
Liam Patterson <liam.patterson94@gmail.com>
2026-05-12 12:44:43 +0200

    sei settings

--- src/deployment/deployment.service.ts
+++ src/deployment/deployment.service.ts
@@ -191,8 +191,8 @@ export class DeploymentService {
         blockchainType: BlockchainType.Sei,
         rpcEndpoint: this.configService.get('SEI_RPC_ENDPOINT'),
         wssEndpoint: this.resolveWssEndpoint('SEI_WSS_ENDPOINT', 'SEI_RPC_ENDPOINT'),
-        harvestEventsBatchSize: 50,
-        harvestConcurrency: 1,
+        harvestEventsBatchSize: 25,
+        harvestConcurrency: 10,
         multicallAddress: '0x51aA24A9230e62CfaF259c47DE3133578cE36317',
         startBlock: 79146720,
         gasToken: {
